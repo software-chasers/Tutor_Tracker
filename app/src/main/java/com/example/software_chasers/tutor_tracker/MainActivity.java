6a0bc5fb06d329a846cc0df0f0166d0fab3839a9
Potego <potegokgaphola98@gmail.com>
2019-03-04 22:39:21 +0200

    Testing the yml file

--- app/src/main/java/com/example/software_chasers/tutor_tracker/MainActivity.java
+++ app/src/main/java/com/example/software_chasers/tutor_tracker/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
+       // Button button = (Button) findViewById(R.id.button);
     }
 }
