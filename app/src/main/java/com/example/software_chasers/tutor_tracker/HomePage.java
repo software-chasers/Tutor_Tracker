e0021aa9837673974a1aabd7c7bda7eca0abfbe2
Potego <potegokgaphola98@gmail.com>
2019-03-09 18:24:25 +0200

    Fixed ic_launcher errors and moved menu button code to HomePage.

--- app/src/main/java/com/example/software_chasers/tutor_tracker/HomePage.java
+++ app/src/main/java/com/example/software_chasers/tutor_tracker/HomePage.java
@@ -12,4 +12,37 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
     }
+
+      /*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+
+            case R.id.item1:
+                Toast.makeText(this,"Item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this,"Item 2 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this,"Item 3 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem1:
+                Toast.makeText(this,"sub Item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem2:
+                Toast.makeText(this,"sub Item 2 selected",Toast.LENGTH_SHORT).show();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+
+    }*/
 }
--- app/src/main/java/com/example/software_chasers/tutor_tracker/MainActivity.java
+++ app/src/main/java/com/example/software_chasers/tutor_tracker/MainActivity.java
@@ -36,37 +36,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.example_menu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId())
-        {
-
-            case R.id.item1:
-                Toast.makeText(this,"Item 1 selected",Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item2:
-                Toast.makeText(this,"Item 2 selected",Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item3:
-                Toast.makeText(this,"Item 3 selected",Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.subitem1:
-                Toast.makeText(this,"sub Item 1 selected",Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.subitem2:
-                Toast.makeText(this,"sub Item 2 selected",Toast.LENGTH_SHORT).show();
-                return true;
-                default:
-                    return super.onOptionsItemSelected(item);
-        }
-
-    }
 }
