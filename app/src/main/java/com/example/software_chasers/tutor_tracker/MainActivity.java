f171800679ea0971f29dfdb0ebcfb3ac7ff654cc
Potego <potegokgaphola98@gmail.com>
2019-03-11 13:22:02 +0200

    Updated homepage

--- app/src/main/java/com/example/software_chasers/tutor_tracker/MainActivity.java
+++ app/src/main/java/com/example/software_chasers/tutor_tracker/MainActivity.java
@@ -3,18 +3,26 @@ package com.example.software_chasers.tutor_tracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button = (Button) findViewById(R.id.button);
+    Button button2 = (Button) findViewById(R.id.button2);
+    EditText editText = (EditText) findViewById(R.id.editText);
+    EditText editText2 = (EditText)findViewById( R.id.editText2 );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
 
-        Button button = (Button) findViewById(R.id.button);
-        Button button2 = (Button) findViewById(R.id.button2);
+        editText2.addTextChangedListener( loginWatcher );
+        editText.addTextChangedListener( loginWatcher );
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,4 +40,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private TextWatcher loginWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String Username = editText.getText().toString().trim();
+            String Password = editText2.getText().toString().trim();
+            button2.setEnabled( !Username.isEmpty() && !Password.isEmpty() );
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
