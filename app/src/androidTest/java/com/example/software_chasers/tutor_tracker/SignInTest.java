15970a475235ef985057e1cc25efd022238e7125
Potego <potegokgaphola98@gmail.com>
2019-03-11 09:50:52 +0200

    added new tests

--- app/src/androidTest/java/com/example/software_chasers/tutor_tracker/SignInTest.java
+++ app/src/androidTest/java/com/example/software_chasers/tutor_tracker/SignInTest.java
@@ -2,11 +2,9 @@ package com.example.software_chasers.tutor_tracker;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import androidx.test.filters.LargeTest;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -14,17 +12,14 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.StringEndsWith.endsWith;
 
-@SuppressWarnings("ALL")
-@RunWith(AndroidJUnit4.class)
-@LargeTest
-public class ButtonTest {
+public class SignInTest {
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
-
     @Test
-    public void press() {
-        onView(withText(endsWith("Sign Up")))
-                .check(matches(isDisplayed()));
+    public void checkText(){
+        ViewInteraction button = onView(withText(endsWith("Sign Up")));
+        button.check(matches(isDisplayed()));
     }
+
 }
