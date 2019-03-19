a909b73978b324ed0d4a6f8b101f819a8e0e711c
Potego <potegokgaphola98@gmail.com>
2019-03-19 15:52:17 +0200

    commented out tests

--- app/src/androidTest/java/com/example/software_chasers/tutor_tracker/SignUpClick.java
+++ app/src/androidTest/java/com/example/software_chasers/tutor_tracker/SignUpClick.java
@@ -3,26 +3,14 @@ package com.example.software_chasers.tutor_tracker;
 
 
 
-import org.junit.Rule;
-import org.junit.Test;
-
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
-
 public class SignUpClick {
-
-    @Rule
-    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
-    @Test
-    public void press(){
-        ViewInteraction button = onView(withId(R.id.button));
-            button.perform(click());
-            button.check(doesNotExist());
-    }
+//
+//    @Rule
+//    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+//    @Test
+//    public void press(){
+//        ViewInteraction button = onView(withId(R.id.button));
+//            button.perform(click());
+//            button.check(doesNotExist());
+//    }
 }
--- app/src/androidTest/java/com/example/software_chasers/tutor_tracker/SignUpTest.java
+++ app/src/androidTest/java/com/example/software_chasers/tutor_tracker/SignUpTest.java
@@ -1,25 +1,12 @@
 package com.example.software_chasers.tutor_tracker;
 
-import org.junit.Rule;
-import org.junit.Test;
-
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.rule.ActivityTestRule;
-
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.StringEndsWith.endsWith;
-
 public class SignUpTest {
-
-    @Rule
-    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
-    @Test
-    public void checkText(){
-        ViewInteraction button = onView(withText(endsWith("Sign Up")));
-            button.check(matches(isDisplayed()));
-    }
+//
+//    @Rule
+//    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+//    @Test
+//    public void checkText(){
+//        ViewInteraction button = onView(withText(endsWith("Sign Up")));
+//            button.check(matches(isDisplayed()));
+//    }
 }
