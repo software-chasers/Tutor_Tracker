73a15ebedbcb91759f6e0583244db01c7a0e5e44
kaira_kvo <mkhabelak6@gmail.com>
2019-04-02 13:08:42 +0200

    Modified the sign up and login.

--- app/src/main/java/com/example/software_chasers/tutor_tracker/MainActivity.java
+++ app/src/main/java/com/example/software_chasers/tutor_tracker/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.software_chasers.tutor_tracker;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +24,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
 
-        button = (Button) findViewById(R.id.button);
-        button2 = (Button) findViewById(R.id.button2);
-        editText = (EditText) findViewById(R.id.editText);
-        editText2 = (EditText)findViewById( R.id.editText2 );
+        button = (Button) findViewById(R.id.signup);
+        button2 = (Button) findViewById(R.id.signIn);
+        editText = (EditText) findViewById(R.id.editpassword);
+        editText2 = (EditText)findViewById( R.id.edituserid);
+
 
 
         editText2.addTextChangedListener( loginWatcher );
@@ -33,8 +37,27 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HomePage.class);
-                startActivity(intent);
+                String Userid = editText2.getText().toString();
+                String Pass = editText.getText().toString();
+                ContentValues params = new ContentValues();
+                params.put("userid", Userid);
+                params.put("password", Pass);
+                @SuppressLint("StaticFieldLeak") AsyncHTTPPost asyncHTTPPost = new AsyncHTTPPost("http://lamp.ms.wits.ac.za/~s1741606/login.php",params) {
+                    @Override
+                    protected void onPostExecute(String output) {
+                    if(output.equals("login Successful")) {
+                        Intent intent = new Intent(MainActivity.this, HomePage.class);
+                        startActivity(intent);
+                        String s = "hi";
+                        Toast.makeText(getApplicationContext(), "Sign in successfully", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Sign in Unsuccessful", Toast.LENGTH_SHORT).show();
+                    }
+
+                    }
+                };
+               asyncHTTPPost.execute();
             }
         });
 
@@ -55,9 +78,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            String Username = editText.getText().toString().trim();
-            String Password = editText2.getText().toString().trim();
-            button2.setEnabled( !Username.isEmpty() && !Password.isEmpty() );
+            String Userid = editText.getText().toString().trim();
+            String  Pass = editText2.getText().toString().trim();
+            button2.setEnabled( !Userid.isEmpty() && !Pass.isEmpty() );
 
         }
 
