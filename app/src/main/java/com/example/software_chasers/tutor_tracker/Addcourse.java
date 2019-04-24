f83549a5b422868e90f5f78fcf7345a5d4b4503a
kaira_kvo <mkhabelak6@gmail.com>
2019-04-24 16:53:45 +0200

    Fixed the lecturer home layout. added functionality to add courses

--- app/src/main/java/com/example/software_chasers/tutor_tracker/Addcourse.java
+++ app/src/main/java/com/example/software_chasers/tutor_tracker/Addcourse.java
@@ -14,7 +14,7 @@ import android.widget.Toast;
 
 public class Addcourse extends Activity {
 
-    private Spinner spinner1, spinner2;
+    private Spinner spinner1;
     private Button btnSubmit;
 
     @Override
@@ -22,40 +22,19 @@ public class Addcourse extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcourse);
 
-        addItemsOnSpinner2();
         addListenerOnButton();
         addListenerOnSpinnerItemSelection();
     }
 
-    // add items into spinner dynamically
-    public void addItemsOnSpinner2() {
-
-        spinner2 = (Spinner) findViewById(R.id.spinner2);
-        List<String> list = new ArrayList<String>();
-        list.add("list 1");
-        list.add("list 2");
-        list.add("list 3");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(dataAdapter);
-    }
-
     public void addListenerOnSpinnerItemSelection() {
         spinner1 = (Spinner) findViewById(R.id.spinner1);
-        spinner1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-        });
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
 
     // get the selected dropdown list value
     public void addListenerOnButton() {
 
         spinner1 = (Spinner) findViewById(R.id.spinner1);
-        spinner2 = (Spinner) findViewById(R.id.spinner2);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
         btnSubmit.setOnClickListener(new OnClickListener() {
@@ -65,8 +44,7 @@ public class Addcourse extends Activity {
 
                 Toast.makeText(Addcourse.this,
                         "OnClickListener : " +
-                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
-                                "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
+                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()),
                         Toast.LENGTH_SHORT).show();
             }
 
